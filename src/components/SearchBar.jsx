1fed1713f178d11e4bc9f99c13ccb18ac6064298
caitmann <caitlynleemann@gmail.com>
2024-03-29 21:43:29 -0400

    commit with UseRef replacing UseState

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,13 @@
-import {useState} from 'react';
+import {useContext} from 'react';
+import {SearchContext} from '../context/SearchContext';
 
-function SearchBar(props) {
-    let [searchTerm, setSearchTerm] = useState('')
+function SearchBar() {
+    const {term, handleSearch} = useContext(SearchContext)
 
     return (
-        <form onSubmit = {(e) => props.handleSearch(e, searchTerm)}>
-            <input type="text" placeholder="Enter a search term here" onChange = {(e) => props.handleSearch(e, e.target.value)} />
-            <input type="submit" />
+        <form>
+            <input ref={term} type="text" placeholder="Enter a search term here" />
+            <button onClick={(e) => handleSearch(e, term.current.value)}>Submit</button>
         </form>
     )
 }
